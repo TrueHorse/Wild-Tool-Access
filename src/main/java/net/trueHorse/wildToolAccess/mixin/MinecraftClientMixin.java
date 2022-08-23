e0bf3de2cb2e57a78379b43df156efd80ae0c32d
Paul.Roß <p_Ross@gmx.de>
2022-08-23 18:20:37 +0200

    converted redirects to injects

--- src/main/java/net/trueHorse/wildToolAccess/mixin/MinecraftClientMixin.java
+++ src/main/java/net/trueHorse/wildToolAccess/mixin/MinecraftClientMixin.java
@@ -3,7 +3,8 @@ package net.trueHorse.wildToolAccess.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -23,13 +24,20 @@ public abstract class MinecraftClientMixin {
     @Shadow
     private void doAttack(){};
     
-    @Redirect(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;doAttack()V"))
-    private void attackOrChoose(MinecraftClient client){
+    @Inject(method = "doAttack", at = @At(value = "HEAD"), cancellable = true)
+    private void attackOrChoose(CallbackInfo info){
         if(((GameOptionsAccess)options).isAccessBarOpen()&&WildToolAccessConfig.getBoolValue("mouseSelect")){
             ((InGameHudAccess)inGameHud).closeOpenAccessbar(true);
-            client.options.keyAttack.setPressed(false);
-        }else{
-            this.doAttack();
+            options.keyAttack.setPressed(false);
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "openPauseMenu", at = @At("HEAD"),cancellable = true)
+    public void pauseMenuOrCloseAccess(boolean bl, CallbackInfo info){
+        if(((GameOptionsAccess)options).isAccessBarOpen()&&WildToolAccessConfig.getBoolValue("escClose")){
+            ((InGameHudAccess)inGameHud).closeOpenAccessbar(false);
+            info.cancel();
         }
     }
 }
