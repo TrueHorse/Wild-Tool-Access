7f6fe5810d4a605a5779d4ab9a80bab6fa7a4229
Paul.Roß <81418739+TrueHorse@users.noreply.github.com>
2022-09-07 17:25:19 +0200

    fixed package name

--- src/main/java/net/trueHorse/wildToolAccess/compatibility/ModMenuIntegration.java
+++ src/main/java/net/trueHorse/wildToolAccess/compatibility/ModMenuIntegration.java
@@ -1,4 +1,4 @@
-package net.trueHorse.wildToolAccess.compatibitity;
+package net.trueHorse.wildToolAccess.compatibility;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
