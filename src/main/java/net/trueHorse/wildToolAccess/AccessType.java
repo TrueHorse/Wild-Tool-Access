24517f10cff5673d38f57342df64003c17161b71
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2022-12-26 16:49:58 +0100

    removed value from enum

--- src/main/java/net/trueHorse/wildToolAccess/AccessType.java
+++ src/main/java/net/trueHorse/wildToolAccess/AccessType.java
@@ -2,16 +2,10 @@ package net.trueHorse.wildToolAccess;
 
 public enum AccessType {
 
-    TOOL("tool"),
-    SWORD("sword"),
-    RANGED_WEAPON("ranged weapon"),
-    POTION("potion"),
-    BUCKET("bucket"),
-    STUFF("stuff");
-
-    public final String id;
-
-    AccessType(String id) {
-        this.id = id;
-    }
+    TOOL(),
+    SWORD(),
+    RANGED_WEAPON(),
+    POTION(),
+    BUCKET(),
+    STUFF()
 }
