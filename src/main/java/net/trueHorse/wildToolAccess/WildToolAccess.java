cfe727c9ec2ad130b2425bac3cfd1b39c7f5ba13
Paul Roß <81418739+TrueHorse@users.noreply.github.com>
2023-12-03 15:29:29 +0100

    fix desync of key and bar status in sticky mode

--- src/main/java/net/trueHorse/wildToolAccess/WildToolAccess.java
+++ src/main/java/net/trueHorse/wildToolAccess/WildToolAccess.java
@@ -73,7 +73,6 @@ public class WildToolAccess
 
         private static boolean access1WasPressed;
         private static boolean access2WasPressed;
-        private static boolean bothWerePressed;
 
         @SubscribeEvent
         public static void onCommandsRegister(RegisterClientCommandsEvent event){
@@ -87,7 +86,6 @@ public class WildToolAccess
 
                 if(!WildToolAccessConfig.toggleMode){
                     if(ACCESS_1_BINDING.get().isDown()&&ACCESS_2_BINDING.get().isDown()) {
-                        bothWerePressed = true;
                         return;
                     }
 
@@ -100,7 +98,6 @@ public class WildToolAccess
 
                     access1WasPressed = ACCESS_1_BINDING.get().isDown();
                     access2WasPressed = ACCESS_2_BINDING.get().isDown();
-                    bothWerePressed = false;
                 }else {
                     while (ACCESS_1_BINDING.get().consumeClick()) {
                         onToggleBarBindingPressed(1, hudAcc);
@@ -116,7 +113,7 @@ public class WildToolAccess
             if (accessBinding.get().isDown()) {
                 hudAcc.openAccessbar(accessBinding==ACCESS_1_BINDING?1:2);
             } else {
-                if(!bothWerePressed) {
+                if(hudAcc.getOpenAccessBar()!=null) {
                     hudAcc.closeOpenAccessbar(true);
                 }
             }
